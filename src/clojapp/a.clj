574c7c2144b135a0832e9ea5609dd54cb9a1a07f
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2014-11-17 01:36:19 +0000

    a star working

--- src/clojapp/a.clj
+++ src/clojapp/a.clj
@@ -42,64 +42,63 @@
 (defn astar [start-yx step-est cell-costs]
   (let [size (count cell-costs)]
     (loop [steps 0
-           routes (vec (repeat size (vec (repeat size nil))))
+           routes (vec (repeat size (vec (repeat size nil)))) ;; i.e. 2-d vecor
            work-todo (sorted-set [0 start-yx])]
       (if (empty? work-todo) ;; check done
-        [(peek (peek routes)) :steps steps]
-        (let [[_ yx :as work-item] (first work-todo)
-              rest-work-todo (disj work-todo work-item)
-              nbr-yxs (neighbors size yx)
+        [(peek (peek routes)) :steps steps] ;; grab first route
+        (let [[_ yx :as work-item] (first work-todo)         ;; get next work item
+              rest-work-todo (disj work-todo work-item)      ;; clear from todo
+              nbr-yxs (neighbors size yx)                    ;; calculate least cost
               cheapest-nbr (min-by :cost
                                    (keep #(get-in routes %)
                                          nbr-yxs))
-              newcost (path-cost (get-in cell-costs yx)
+              newcost (path-cost (get-in cell-costs yx)      ;; calculate path so far
                                  cheapest-nbr)
               oldcost (:cost (get-in routes yx))]
-          (if (and oldcost (>= newcost oldcost))
+          (if (and oldcost (>= newcost oldcost))                ;; check if new is worse
             (recur (inc steps) routes rest-work-todo)
-            (recur (inc steps)
+            (recur (inc steps) ;; place new path in routes
                    (assoc-in routes yx
                              {:cost newcost
                               :yxs (conj (:yxs cheapest-nbr [])
                                          yx)})
-                   (into rest-work-todo
+                   (into rest-work-todo ;; add estimated path to todo (sorted-set) and recur
                          (map
-                          (fn [w] ;; expects w = [y x] is a Vector
+                          (fn [w]
                             (let [[y x] w]
                               [(total-cost newcost step-est size y x) w]))
                           nbr-yxs)))))))))
 
-
+;; run it!
 (astar [0 0]
        900
        world)
 
 
-
 ;; ============ NOTE on comparable ======================
 
-(into (sorted-set [0 [5 5]])
-      [[0 [5 6]]]) ;; ok
+;; (into (sorted-set [0 [5 5]])
+;;       [[0 [5 6]]]) ;; ok
 
-(into (sorted-set [0 [5 5]])
-      [[0 (lazy-seq [5 6])]]) ;; NO!!! clojure.lang.Lazy cannot be cast to java.lang.Comparable
+;; (into (sorted-set [0 [5 5]])
+;;       [[0 (lazy-seq [5 6])]]) ;; NO!!! clojure.lang.Lazy cannot be cast to java.lang.Comparable
 
-(into (sorted-set [0 [5 5]])
-      [[0 ['(5 6)]]]) ;; ok
+;; (into (sorted-set [0 [5 5]])
+;;       [[0 ['(5 6)]]]) ;; ok
 
-(into (sorted-set [0 [5 5]])
-      [[0 '((5 6))]]) ;; NOO!  clojure.lang.PersistentList cannot be cast to java.lang.Comparable
+;; (into (sorted-set [0 [5 5]])
+;;       [[0 '((5 6))]]) ;; NOO!  clojure.lang.PersistentList cannot be cast to java.lang.Comparable
 
 
-(instance? Comparable (first (lazy-seq [5 6])))
+;; (instance? Comparable (first (lazy-seq [5 6])))
 
-(every? #(instance? Comparable %) (lazy-seq [5 6])) ;; true
-(every? #(instance? Comparable %) [[5 6]])          ;; true
-(every? #(instance? Comparable %) ['(5 6)])         ;; false
+;; (every? #(instance? Comparable %) (lazy-seq [5 6])) ;; true
+;; (every? #(instance? Comparable %) [[5 6]])          ;; true
+;; (every? #(instance? Comparable %) ['(5 6)])         ;; false
 
-(type '(5 6)) ;; clojure.lang.PersistentList  - NOT comparable!
-(type [[5 6]]) ;; clojure.lang.PersistentVector - is comparable
-(type (vec '(5 6))) ;; clojure.langPersistentVector - comparable
+;; (type '(5 6)) ;; clojure.lang.PersistentList  - NOT comparable!
+;; (type [[5 6]]) ;; clojure.lang.PersistentVector - is comparable
+;; (type (vec '(5 6))) ;; clojure.langPersistentVector - comparable
 
 ;; Moral of the story:
 ;; Lists are NOT comparable. Vectors are
@@ -110,5 +109,5 @@
         {:bar 55 :foo 2}
         {:foo 1 :bar 77}])
 
+;;(sort-by #(vec (map % [:foo :bar])) x)
 
-(sort-by #(vec (map % [:foo :bar])) x)
